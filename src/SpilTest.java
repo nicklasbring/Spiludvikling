4850dd51022cd0846dc44febff73565ca4840d7d
Nicklas Bring Nielsen <nicklas.bring.nielsen@gmail.com>
2019-01-07 14:38:42 +0100

    Tilføjer onAction

--- src/SpilTest.java
+++ src/SpilTest.java
@@ -1,15 +1,50 @@
 import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.entity.Entities;
+import com.almasb.fxgl.input.Input;
+import com.almasb.fxgl.input.UserAction;
 import com.almasb.fxgl.settings.GameSettings;
+import com.almasb.fxgl.entity.Entity;
+import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
-    public class SpilTest extends GameApplication {
+import java.awt.*;
+
+public class SpilTest extends GameApplication {
 
         @Override
         protected void initSettings(GameSettings gameSettings) {
-            gameSettings.setWidth(1280);
-            gameSettings.setHeight(720);
+            gameSettings.setWidth(500);
+            gameSettings.setHeight(500);
             gameSettings.setTitle("Spiltest");
+        }
+
+        private Entity player;
+
+        @Override
+        protected void initGame() {
+            player = Entities.builder()
+                                .at(250, 250)
+                                .viewFromNode(new Rectangle(25, 25, Color.GREEN))
+                                .buildAndAttach(getGameWorld());
+            }
 
 
+        @Override
+        protected void initInput() {
+            Input input = getInput();
+
+            input.addAction(new UserAction("Højre"){
+                @Override
+                protected void onAcion(){
+                    player.translateX(5); //Rykker 5 pixel til højre
+                }
+            }, KeyCode.D);
+        }
+
+
+    public static void main(String[] args) {
+            launch(args);
         }
     }
 
