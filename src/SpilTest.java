bd68faf0d1f322b03a69718969d47d3a326fc980
Nicklas Bring Nielsen <nicklas.bring.nielsen@gmail.com>
2019-01-07 11:54:28 +0100

    Initial commit

---
+++ src/SpilTest.java
@@ -0,0 +1,14 @@
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.settings.GameSettings;
+
+    public class SpilTest extends GameApplication {
+
+        @Override
+        protected void initSettings(GameSettings gameSettings) {
+            gameSettings.setWidth(1280);
+            gameSettings.setHeight(720);
+            gameSettings.setTitle("Spiltest");
+
+        }
+    }
+
